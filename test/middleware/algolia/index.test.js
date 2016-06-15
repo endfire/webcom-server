f6e9943f23c0671437f27d332de95d02f1b9eacb
Dylan <dylanslack@gmail.com>
2016-06-15 13:10:39 -0600

    Add Algolia middleware and test files

---
+++ test/middleware/algolia/index.test.js
@@ -0,0 +1,49 @@
+import test from 'ava';
+import algolia from '../../../src/middleware/algolia';
+
+test('algolia', async t => {
+  const assertPost = res => {
+    t.truthy(res.createdAt, 'createdAt key is present');
+    t.is(+res.objectID, 1, 'created object has correct id');
+  };
+
+  await algolia({
+    method: 'post',
+    body: {
+      id: 1,
+      name: 'Dylan',
+      email: 'dylanslack@gmail.com',
+      pets: [1, 2],
+      company: 1,
+    },
+  }, assertPost);
+
+  const assertPatch = res => {
+    t.truthy(res.updatedAt, 'updatedAt key is present');
+    t.is(+res.objectID, 1, 'updated object has correct id');
+  };
+
+  await algolia({
+    method: 'patch',
+    body: {
+      id: 1,
+      name: 'Dy-lon',
+    },
+  }, assertPatch);
+
+  const assertDelete = res => {
+    t.truthy(res.deletedAt, 'deletedAt key is presrent');
+    t.is(+res.objectID, 1, 'updated object has correct id');
+  };
+
+  await algolia({
+    method: 'delete',
+    body: { id: 1 },
+  }, assertDelete);
+
+  const assertInvalidMethod = res => t.falsy(res, 'should have no argument');
+
+  await algolia({
+    method: 'invalid-method',
+  }, assertInvalidMethod);
+});
