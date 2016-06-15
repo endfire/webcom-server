24abdcc23157501aeabc1ac2ec1114831d8c619f
Dylan <dylanslack@gmail.com>
2016-06-15 15:23:58 -0600

    Export database singleton instead of class

--- test/services/database/index.test.js
+++ test/services/database/index.test.js
@@ -4,7 +4,7 @@ import { schemas } from '../../fixtures';
 
 const db = new Database(schemas, {
   host: '107.170.131.151',
-  db: 'test',
+  name: 'test',
 });
 
 test.before('connect', async t => {
