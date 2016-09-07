3b84a245fc500df4e69a7ddc8665c154efdbdcf2
calvinbrewer <brewercalvinj@gmail.com>
2016-09-07 15:04:29 -0600

    Remove image and algolia middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,14 +1,12 @@
 /* eslint-disable no-param-reassign */
-import algolia from './algolia';
 import authenticate from './authenticate';
 import authorize from './authorize';
-import image from './image';
 import read from './read';
 import write from './write';
 import download from './download';
 import start from './start';
 
-export const writeMiddleware = [start, authorize, image, write, algolia];
+export const writeMiddleware = [start, authorize, write];
 export const readMiddleware = [start, authorize, read];
 export const authenticateMiddleware = [start, authenticate];
 export const downloadMiddleware = [authorize, download];
