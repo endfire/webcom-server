d38996f8cca572ecda72a067670705e342f1f357
calvinbrewer <brewercalvinj@gmail.com>
2016-09-19 12:40:29 -0600

    Add redink user and password

--- src/index.js
+++ src/index.js
@@ -6,16 +6,20 @@ import app from './application';
 const {
     RETHINKDB_URL: host,
     RETHINKDB_NAME: name,
+    REDINK_USER: user,
+    REDINK_PASSWORD: password,
     PORT: port = 4200,
 } = process.env;
 
 const db = redink();
 
-db.start({ schemas, host, name })
+db.start({ schemas, host, name, user, password })
   .then(() => {
     app.listen(port);
     console.log(`Server started on port ${port}.`);
   })
   .catch(err => {
+    console.log('I got an error');
+    console.log(err);
     throw err;
   });
